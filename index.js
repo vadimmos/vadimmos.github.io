b71b25621dca6da60aab4caa67ad8a2e3e488e17
vadimmos <vadimmos@ya.ru>
2021-02-08 00:15:31 +0300

    fix sw

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const reg = await navigator.serviceWorker.register('./sw.js')
+      const reg = await navigator.serviceWorker.register('/sw.js');
       console.log('Service worker successfully registered', reg);
     } catch (err) {
       console.error(err);
