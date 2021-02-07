9cc698ccf9ba776655336777076f178830a48850
vadimmos <vadimmos@ya.ru>
2021-02-07 23:58:53 +0300

    add sw

--- sw.js
+++ sw.js
@@ -1,34 +1,54 @@
-const CACHE = 'cache-and-update-v1';
+const staticCacheName = 'static-cache-v1';
+const dynamicCacheName = 'dynamic-cache-v1';
+const staticAssets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/icons/icon-128x128.png',
+  '/images/icons/icon-192x192.png',
+  '/style.css',
+  '/components/',
+  '/components/vb-header/',
+  '/components/vb-header/vb-header.js',
+];
+self.addEventListener('install', async e => {
+  const preCache = async () => {
+    const cache = await caches.open(staticCacheName);
+    return await cache.addAll(staticAssets);
+  };
+  e.waitUntil(preCache());
 
-self.addEventListener('install', (event) => {
-  console.log('Установлен');
-  event.waitUntil(
-    caches.open(CACHE).then((cache) =>
-      cache.addAll(['/']))
-  );
 });
-
-self.addEventListener('activate', (event) => {
-  console.log('Активирован');
+self.addEventListener('activate', async e => {
+  const cacheKeys = await caches.keys();
+  const checkKeys = cacheKeys.map(async key => {
+    if (staticCacheName !== key) {
+      await caches.delete(key);
+    }
+  })
+  await Promise.all(checkKeys);
 });
-
-self.addEventListener('fetch', function (event) {
-  console.log('Запрос');
-  event.respondWith(fromCache(event.request));
-  event.waitUntil(update(event.request));
+self.addEventListener('fetch', e => {
+  e.respondWith(checkCache(e.request));
 });
-
-function fromCache(request) {
-  return caches.open(CACHE).then((cache) =>
-    cache.match(request).then((matching) =>
-      matching || Promise.reject('no-match')
-    ));
+/**
+ * @param {Request} req
+ */
+async function checkCache(req) {
+  const url = new URL(req.url);
+  if (url.pathname === '/') {
+    req = new Request(`${url.origin}/`);
+  }
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || checkOnline(req);
 }
-
-function update(request) {
-  return caches.open(CACHE).then((cache) =>
-    fetch(request).then((response) =>
-      cache.put(request, response)
-    )
-  );
+async function checkOnline(req) {
+  const cache = await caches.open(dynamicCacheName);
+  try {
+    const res = await fetch(req);
+    await cache.put(req, res.clone());
+    return res;
+  } catch (err) {
+    return await cache.match(req);
+  }
 }
