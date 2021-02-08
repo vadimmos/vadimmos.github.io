59072192f754d5fae539d6d5d1de5c745449d67b
vadimmos <vadimmos@ya.ru>
2021-02-08 12:11:17 +0300

    add detector debug

--- detector/detector.js
+++ detector/detector.js
@@ -2,6 +2,26 @@ const indicator = document.getElementById('indicator');
 if (indicator && typeof Gyroscope === "function") {
   window.addEventListener("deviceorientation", handleOrientation, true);
 }
+let debug = null;
+let id = 0;
+let count = 0;
+function cheat() {
+  clearTimeout(id);
+  count++;
+  id = setTimeout(() => {
+    if (count >= 10) {
+      window.removeEventListener('click', cheat);
+      debug = document.createElement('div');
+      debug.style.setProperty('position', 'fixed');
+      debug.style.setProperty('top', '100px');
+      debug.style.setProperty('left', '45%');
+      debug.style.setProperty('color', 'red');
+      document.body.appendChild(debug);
+    }
+    count = 0;
+  }, 1000);
+}
+window.addEventListener('click', cheat);
 
 function handleOrientation(event) {
   const absolute = event.absolute;
@@ -14,11 +34,14 @@ function handleOrientation(event) {
 
   let bg = '0'
 
-  for(let deg = -180, idx = 0;deg < 180; deg += 45, idx++){
-    if (alpha >= deg && alpha < deg + 45){
+  for (let deg = -180, idx = 0; deg < 180; deg += 45, idx++) {
+    if (alpha >= deg && alpha < deg + 45) {
       bg = idx.toString();
       break;
     }
   }
-    indicator.style.setProperty('background-image', `url("./img/${bg}.png")`);
+  if (debug) {
+    debug.textContent = alpha;
   }
+  indicator.style.setProperty('background-image', `url("./img/${bg}.png")`);
+}
