04b288a8cef0bf36da88bab514b245b24c0401c1
vadimmos <vadimmos@ya.ru>
2021-02-09 00:15:33 +0300

    Revert "sw update"
    This reverts commit 287f94f9c2640b947f7ac32377c7cff17e0b34b5.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'static-cache-v2';
-const dynamicCacheName = 'dynamic-cache-v2';
+const staticCacheName = 'static-cache-v1';
+const dynamicCacheName = 'dynamic-cache-v1';
 const staticAssets = [
   '/',
   '/index.html',
@@ -38,8 +38,7 @@ async function checkCache(req) {
     req = new Request(`${url.origin}/`);
   }
   const cachedResponse = await caches.match(req);
-  const online = await checkOnline(req);
-  return cachedResponse || online;
+  return cachedResponse || checkOnline(req);
 }
 async function checkOnline(req) {
   const cache = await caches.open(dynamicCacheName);
