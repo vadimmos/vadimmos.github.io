27b85e59847e04394b684657d9849235b5a4d646
vadimmos <vadimmos@ya.ru>
2021-04-07 00:28:20 +0300

    update qr

--- serverless-webrtc-test/index.js
+++ serverless-webrtc-test/index.js
@@ -4,58 +4,70 @@ const main = document.querySelector('main');
 main.style.setProperty('flex-direction', 'column');
 main.style.setProperty('align-items', 'center');
 
+const video = document.createElement('video');
+
+const cnv = document.createElement('canvas');
+cnv.style.setProperty('width', '256px');
+cnv.style.setProperty('height', '256px');
+cnv.width = 256;
+cnv.height = 256;
+video.style.setProperty('width', '256px');
+video.style.setProperty('height', '256px');
+
 const qr = document.createElement('qr-code');
 main.appendChild(qr);
 qr.data = 'какой-то текст';
 
+const btn = document.createElement('button');
+btn.textContent = 'START';
+btn.addEventListener('click', () => start());
+main.appendChild(btn);
 
-if (!('BarcodeDetector' in window)) {
-  alert('нету BarcodeDetector');
-} else {
-  const video = document.createElement('video');
-  
-  const cnv = document.createElement('canvas');
-  cnv.style.setProperty('width', '256px');
-  cnv.style.setProperty('height', '256px');
-  video.style.setProperty('width', '256px');
-  video.style.setProperty('height', '256px');
-  main.appendChild(video);
-  main.appendChild(cnv);
-  function drawCanvas(img) {
-    cnv.width = parseInt(getComputedStyle(cnv).width.split('px')[0]);
-    cnv.height = parseInt(getComputedStyle(cnv).height.split('px')[0]);
-    let ratio = Math.min(cnv.width / img.width, cnv.height / img.height);
-    let x = (cnv.width - img.width * ratio) / 2;
-    let y = (cnv.height - img.height * ratio) / 2;
-    ctx.clearRect(0, 0, cnv.width, cnv.height);
-    ctx.drawImage(img, 0, 0, img.width, img.height,
-      x, y, img.width * ratio, img.height * ratio);
-    return cnv.toDataURL();
+
+function start(){
+  if (!('BarcodeDetector' in window)) {
+    alert('нету BarcodeDetector');
+  } else {
+    btn.disabled = true;
+    main.appendChild(video);
+    main.appendChild(cnv);
+    function drawCanvas(img) {
+      let ratio = Math.min(cnv.width / img.width, cnv.height / img.height);
+      let x = (cnv.width - img.width * ratio) / 2;
+      let y = (cnv.height - img.height * ratio) / 2;
+      ctx.clearRect(0, 0, cnv.width, cnv.height);
+      ctx.drawImage(img, 0, 0, img.width, img.height,
+        x, y, img.width * ratio, img.height * ratio);
+      return cnv.toDataURL();
+    }
+    const ctx = cnv.getContext('2d');
+    const img = new Image();
+    main.appendChild(img);
+    navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
+      .then(async (stream) => {
+        video.srcObject = stream;
+        video.play();
+
+        const track = stream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(track);
+        const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
+
+        const interval = setInterval(async () => {
+          const imageBitmap = await imageCapture.grabFrame();
+          img.src = drawCanvas(imageBitmap);
+          const barcodes = await barcodeDetector.detect(img);
+          barcodes.forEach(barcode => {
+            if (barcode.rawData) {
+              clearInterval(interval);
+              btn.disabled = false;
+              alert(barcode.rawData);
+            }
+          });
+        }, 300);
+      })
+      .catch(function (err) {
+        console.log("An error occurred: " + err);
+      });
   }
-  const ctx = cnv.getContext('2d');
-  const img = new Image();
-  main.appendChild(img);
-  navigator.mediaDevices.getUserMedia({ video: { facingMode: { exact: "environment" } }, audio: false })
-    .then(async (stream) => {
-      video.srcObject = stream;
-      video.play();
-
-      const track = stream.getVideoTracks()[0];
-      const imageCapture = new ImageCapture(track);
-      const barcodeDetector = new BarcodeDetector({ formats: ['qr_code'] });
-
-      const interval = setInterval(async () => {
-        const imageBitmap = await imageCapture.grabFrame();
-        img.src = drawCanvas(imageBitmap);
-        const barcodes = await barcodeDetector.detect(img);
-        barcodes.forEach(barcode => {
-          clearInterval(interval);
-          alert(barcode.rawData);
-        });
-      }, 300);
-    })
-    .catch(function (err) {
-      console.log("An error occurred: " + err);
-    });
 }
 
