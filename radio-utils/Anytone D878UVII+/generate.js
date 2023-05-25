0a8ecc42b445ebce0db01bfb379e6fe26ba845e7
vadimmos <vadimmos@ya.ru>
2023-05-25 13:24:26 +0300

    start idx 0 -> 1

--- radio-utils/Anytone D878UVII+/generate.js
+++ radio-utils/Anytone D878UVII+/generate.js
@@ -4,8 +4,8 @@ if (get_btn instanceof HTMLButtonElement) {
   get_btn.addEventListener('click', () => {
     let result = '';
     result += '"id","num","aeskey"';
-    for (let i = 0; i < 256; i++){
-      result += `\n"${i}","64","${gen64x16()}"`;
+    for (let i = 1; i < 256; i++){
+      result += `\n\r"${i}","64","${gen64x16()}"`;
     }
     const a = document.createElement('a');
     a.href = URL.createObjectURL(new Blob([result], {type: 'text/csv'}));
