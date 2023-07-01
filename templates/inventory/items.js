c5c1ac4b92c8c5822d47544938711dd3009c3aad
vadimmos <vadimmos@ya.ru>
2023-07-01 19:23:58 +0300

    inventory

---
+++ templates/inventory/items.js
@@ -0,0 +1,108 @@
+import { naturalCompare } from "./funcs.js";
+
+export default [
+  {
+    icon: './icons/ico_01.png',
+    name: 'Калаш-хуяш',
+    weight: 4.5,
+    can_stack: false,
+    count: 1,
+    type: 'weapon'
+  },
+  {
+    icon: './icons/ico_02.png',
+    name: 'Пистик-хуистик',
+    weight: 1.5,
+    can_stack: false,
+    count: 1,
+    type: 'weapon'
+  },
+  {
+    icon: './icons/ico_03.png',
+    name: 'Ножик-хуёжик',
+    weight: 0.5,
+    can_stack: false,
+    count: 1,
+    type: 'weapon'
+  },
+  {
+    icon: './icons/ico_04.png',
+    name: 'Фасоль-хуёль',
+    weight: 1.0,
+    can_stack: true,
+    count: 3,
+    type: 'food'
+  },
+  {
+    icon: './icons/ico_05.png',
+    name: 'Тушёнка-хуёнка',
+    weight: 1.0,
+    can_stack: true,
+    count: 1,
+    type: 'food'
+  },
+  {
+    icon: './icons/ico_06.png',
+    name: 'Граната-хуята',
+    weight: 0.5,
+    can_stack: true,
+    count: 2,
+    type: 'weapon'
+  },
+  {
+    icon: './icons/ico_07.png',
+    name: 'Аптечка-хуечка',
+    weight: 0.25,
+    can_stack: true,
+    count: 2,
+    type: 'medic'
+  },
+  {
+    icon: './icons/ico_08.png',
+    name: 'патроны-хуёны 8x18',
+    weight: 0.2,
+    can_stack: true,
+    count: 30,
+    type: 'ammo'
+  },
+  {
+    icon: './icons/ico_09.png',
+    name: 'Бинт-хуинт',
+    weight: 0.1,
+    can_stack: true,
+    count: 10,
+    type: 'medic'
+  },
+  {
+    icon: './icons/ico_10.png',
+    name: 'Детали-хуяли',
+    weight: 0.01,
+    can_stack: true,
+    count: 30,
+    type: 'trash'
+  },
+  {
+    icon: './icons/ico_11.png',
+    name: 'патроны-хуёны 7x62',
+    weight: 0.3,
+    can_stack: true,
+    count: 100,
+    type: 'ammo'
+  },
+  {
+    icon: './icons/ico_12.png',
+    name: 'Провода-хуеда',
+    weight: 0.01,
+    can_stack: true,
+    count: 8,
+    type: 'trash'
+  },
+  {
+    icon: './icons/ico_13.png',
+    name: 'Рация-хуяция',
+    weight: 1,
+    can_stack: false,
+    count: 1,
+    type: 'other'
+  },
+].sort((a, b) => naturalCompare(a.name, b.name));
