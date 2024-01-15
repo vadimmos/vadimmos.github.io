22d4c84f83e9113de3b36ddd239f6909947d5a66
vadimmos <vadimmos@ya.ru>
2024-01-15 21:10:21 +0000

    add freq2chan

---
+++ radio-utils/freq2chan/script.js
@@ -0,0 +1,134 @@
+const body = document.body;
+
+const input = document.getElementById('input');
+const datalist = document.getElementById('frequencies');
+const thead = document.getElementById('thead');
+const tbody = document.getElementById('tbody');
+
+function get (node, key = 'root', idx = 0) {
+  const span = document.createElement('span');
+  span.textContent = node[key];
+  return span;
+};
+function load (node, idx = 0, key = 'root') {
+  return localStorage.getItem(`${location.pathname}/${idx}/${key}`) || '';
+};
+function save (node, idx = 0, key = 'root', value) {
+  return localStorage.setItem(`${location.pathname}/${idx}/${key}`, value);
+};
+
+const header = {
+  _idx: {},
+  _value: {},
+  label: {
+    label: 'Частота',
+  },
+  chanel: {
+    label: 'Канал',
+  },
+  description: {
+    label: 'Заметка',
+    render(node, key, idx) {
+      const span = document.createElement('span');
+      span.textContent = load(node, idx, key) || '-';
+      return span;
+    }
+  },
+  tools: {
+    label: '☰',
+    render(node, key, idx) {
+      const tools = node[key];
+      const div = document.createElement('div');
+      div.classList.add('flex');
+      for (const t of tools) {
+        const btn = document.createElement('button');
+        btn.textContent = t.label;
+        btn.title = t.title;
+        btn.addEventListener('click', () => {
+          t.fn();
+        });
+        div.appendChild(btn);
+      }
+      return div;
+    }
+  }
+
+};
+
+const data = [];
+
+let a = 433;
+let b = 0;
+for (let i = 0; i < 69; i++) {
+  b += 25;
+  if (b >= 1000) {
+    a++;
+    b = 0;
+  }
+  const whole = a.toString();
+  const fract = b.toString().padStart(3, '0');
+  const value = parseFloat(`${whole}.${fract.toString().padStart(3, '0')}`);
+  const node = {
+    _idx: i,
+    _value: value,
+    label: value.toString().padEnd(7, '0'),
+    chanel: i,
+    description: localStorage.getItem(`${location.pathname}/${i}/description`) || '',
+    tools: [
+      {
+        label: '🖊',
+        title: 'Редактировать заметку',
+        fn() {
+          const value = prompt('Редактировать заметку', load(node, node._idx, 'description'));
+          if (value !== null) {
+            save(node, node._idx, 'description', value);
+            drawTable();
+          }
+        }
+      }
+    ]
+  };
+  data.push(node);
+  const option = document.createElement('option');
+  option.value = node.label;
+  datalist.appendChild(option);
+}
+drawTable();
+
+let updateId = 0;
+input.addEventListener('input', () => {
+  clearTimeout(updateId);
+  updateId = setTimeout(() => {
+    drawTable();
+  }, 300);
+});
+
+function drawTable(filter = input.value) {
+  requestAnimationFrame(() => {
+    thead.innerHTML = '';
+    const headRow = document.createElement('tr');
+    for (const k in header) {
+      if (k.startsWith('_')) continue;
+      const val = header[k];
+      const col = document.createElement('th');
+      col.textContent = val.label;
+      headRow.appendChild(col);
+    }
+    thead.appendChild(headRow);
+    tbody.innerHTML = '';
+    (filter
+      ? data.filter((r) => r.label.toString().includes(filter))
+      : [...data]
+    ).map((r) => {
+      const row = document.createElement('tr');
+      for (const k in header) {
+        if (k.startsWith('_')) continue;
+        const col = document.createElement('td');
+        const idx = r._idx;
+        col.appendChild(header[k].render?.(r, k, idx) || get(r, k, idx));
+        row.appendChild(col);
+      }
+      tbody.appendChild(row);
+    });
+  });
+}
